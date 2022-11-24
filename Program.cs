b0e2b809d05a3ac3b1a79bc499daa77f7f4ce5d9
KirillDimitrievLabs <45212350+KirillDimitrievLabs@users.noreply.github.com>
2022-11-24 03:42:18 +0300

    NextBadCommit

--- Program.cs
+++ Program.cs
@@ -15,11 +15,9 @@ builder.Services.AddTransient<TokenService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
